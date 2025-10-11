0739517e069f15f6eeecdeb10205cd58f3729650
Shahbozbek Hakimov <hakimovshahbozbek@gmail.com>
2025-10-10 20:35:58 -0400

    Simplifying code and function

--- sliter.bpf.c
+++ sliter.bpf.c
@@ -1,16 +1,16 @@
-#include "network.h"
 #include <bcc/proto.h>
 #include <net/sock.h>
 #include <uapi/linux/ptrace.h>
-#include "packet.h"
-
-
+#include <linux/pkt_cls.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
 
 #define IP_TCP 6
 #define ETH_LEN 14
 #define CONNOUT 10000000000000
 #define IDLEOUT 10000000000000
 #define MAX_MSG_SIZE 1024
+
 
 struct Key {
 	u32 src_ip;
@@ -20,152 +20,77 @@ struct Key {
 };
 
 struct Leaf {
-	int timestamp;
+	unsigned long long prv_comm;
+	unsigned long long first_comm;
 };
 
 
 BPF_HASH(sessions, struct Key, struct Leaf);
-BPF_HASH(connection, u32);
-BPF_HASH(idle, u32);
-
-
-
 
 int xdp(struct xdp_md *ctx) {
 	void *data = (void *)(long)ctx->data;
 	void *data_end = (void *)(long)ctx->data_end;
-
-	char addrArr[6];
-	unsigned long long connTime = bpf_ktime_get_ns();
-
-	long protocol = lookup_protocol(data, data_end);
-	unsigned int srcAddr = lookup_src_addr(data, data_end);
-	int port = tcp_lookup_port(data, data_end);
-
-	if (protocol != 6) {
-        	return XDP_PASS;
-  	}
-
-	if (port != 8080) {
-		return XDP_PASS;
-	}
-
-	unsigned long long *connFirst = connection.lookup(&srcAddr);
-	unsigned long long *idlePrev = idle.lookup(&srcAddr);
-	if (connFirst == NULL) {
-		connection.update(&srcAddr, &connTime);
-	}
-	else{
-		unsigned long long connElapsed = (connTime - *connFirst)/1000;
-		bpf_trace_printk("Elapsed Time Since First Packet: %ld\n", connElapsed);
-		if (idlePrev != NULL){
-			unsigned long long idleElapsed = (connTime - *idlePrev)/1000;
-			bpf_trace_printk("Elapsed Time Since Last Packet: %ld\n", idleElapsed);
-			if (idleElapsed > IDLEOUT)
-                        return XDP_DROP;
-		}
-		//USE CONFIG FILE FOR TIMEOUT TIMES
-		if (connElapsed > CONNOUT)
-                        return XDP_DROP;
-	}
-	idle.update(&srcAddr, &connTime);
-
-	bpf_trace_printk("GOT PORT 80 PACKET");
-	bpf_trace_printk("%ld \n", srcAddr);
-
-	return XDP_PASS;
-}
-
-//TC:
-
-int http_filter(struct __sk_buff *skb){
-	u8 * cursor = 0;
-
-	struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(*ethernet));
-	if (!(ethernet->type == 0x800)) {
-		return -1;
-	}
-
-	struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));
-	if (ip->nextp != IP_TCP) {
-		return -1;
-	}
-
-
 	u32 tcp_header_length = 0;
 	u32 ip_header_length = 0;
 	u32 payload_offset = 0;
 	u32 payload_length = 0;
+	unsigned long long commTime = bpf_ktime_get_ns();
 	struct Key key;
-	struct Leaf zero = {0};
+	struct Leaf leaf;
 
-	struct tcp_t *tcp = cursor_advance(cursor, sizeof(*tcp));
 
-	key.dst_ip = ip->dst;
-	key.src_ip = ip->src;
-	key.dst_port = tcp->dst_port;
-	key.src_port =tcp->src_port;
+	struct ethhdr *ethernet = data;
+	if (data + sizeof(struct ethhdr) <= data_end)
+		return XDP_PASS;
+	if (bpf_ntohs(ethernet->h_proto) != 0x0800){
+		return XDP_PASS;
+	}
+	struct iphdr *ip = data + sizeof(*ethernet);
+	if (data + sizeof(*ethernet) + sizeof(struct iphdr) <= data_end)
+		return XDP_PASS;
+	if (ip->protocol != IPPROTO_TCP){
+		return XDP_PASS;
+	}
+	struct tcphdr *tcp = data + sizeof(*ethernet) + sizeof(*ip);
+	if (data + sizeof(*ethernet) + sizeof(*ip) + sizeof(struct tcphdr) <= data_end)
+		return XDP_PASS;
+
+
+	key.dst_ip = ip->daddr;
+	key.src_ip = ip->saddr;
+	key.dst_port = bpf_ntohs(tcp->dest);
+	key.src_port = bpf_ntohs(tcp->source);
 
 	ip_header_length = ip->hlen << 2;
 	tcp_header_length = tcp->offset << 2;
 
-	payload_offset = ETH_HLEN  + ip_header_length + tcp_header_length;
+	payload_offset = ETH_HLEN + ip_header_length + tcp_header_length;
 	payload_length = ip->tlen - ip_header_length - tcp_header_length;
 
-
-	if (payload_length < 7) {
-		return -1;
+	if (payload_length < 7){
+		return XDP_PASS;
 	}
 
-
-	unsigned long p[7];
-	int i = 0;
-	int j = 0;
-	for (i = payload_offset ; i < (payload_offset + 7) ; i++) {
-		p[j] = load_byte(skb , i);
-		j++;
+	struct Leaf *commCheck = sessions.lookup(&key);
+	if (commCheck == NULL){
+		leaf.prv_comm = commTime;
+		leaf.first_comm = commTime;
+		sessions.update(&key, &leaf);
+	}
+	else{
+		unsigned long long elapsedSinceFirst = (commTime - commCheck->first_comm)/1000000000;
+		unsigned long long elapsedSinceLast = (commTime - commCheck->prv_comm)/1000000000;
+		bpf_trace_printk("Elapsed Time Since First Packet: %ld\n", elapsedSinceFirst);
+		bpf_trace_printk("Elapsed Time Since Previous Packet: %ld\n", elapsedSinceLast);
+		if (elapsedSinceFirst > IDLEOUT || elapsedSinceLast > CONNOUT)
+			return XDP_DROP;
+		leaf.prv_comm = commTime;
+		leaf.first_comm = commCheck->first_comm;
+		sessions.update(&key, &leaf);
 	}
 
-	//find a match with an HTTP message
-	//HTTP
-	if ((p[0] == 'H') && (p[1] == 'T') && (p[2] == 'T') && (p[3] == 'P')) {
-		goto HTTP_MATCH;
-	}
-	//GET
-	if ((p[0] == 'G') && (p[1] == 'E') && (p[2] == 'T')) {
-		goto HTTP_MATCH;
-	}
-	//POST
-	if ((p[0] == 'P') && (p[1] == 'O') && (p[2] == 'S') && (p[3] == 'T')) {
-		goto HTTP_MATCH;
-	}
-	//PUT
-	if ((p[0] == 'P') && (p[1] == 'U') && (p[2] == 'T')) {
-		goto HTTP_MATCH;
-	}
-	//DELETE
-	if ((p[0] == 'D') && (p[1] == 'E') && (p[2] == 'L') && (p[3] == 'E') && (p[4] == 'T') && (p[5] == 'E')) {
-		goto HTTP_MATCH;
-	}
-	//HEAD
-	if ((p[0] == 'H') && (p[1] == 'E') && (p[2] == 'A') && (p[3] == 'D')) {
-		goto HTTP_MATCH;
-	}
+	bpf_trace_printk("GOT PORT 80 PACKET");
 
-	struct Leaf * lookup_leaf = sessions.lookup(&key);
-	if(lookup_leaf) {
-		//send packet to userspace
-		return 0;
-	}
-	return -1;
-
-	HTTP_MATCH:
-	//if not already present, insert into map <Key, Leaf>
-		sessions.lookup_or_init(&key,&zero);
-
-
-	return 0;
+	return XDP_PASS;
 }
-
-
 
